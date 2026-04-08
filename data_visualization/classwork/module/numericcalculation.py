6d7dcb614e146353fe5fe91352b05819cfc19ddf
kajal mishra <km3826219@gmail.com>
2026-04-08 11:03:10 +0530

    Add files via upload

---
+++ data_visualization/classwork/module/numericcalculation.py
@@ -0,0 +1,12 @@
+#function to add two number
+def calculateSum(x,y):
+  return(x+y)
+#function to sub two number
+def calculateSub(x,y):
+  return(x-y)
+#function to mul two number
+def calculateMul(x,y):
+  return(x*y)
+#function to div two number
+def calculateDiv(x,y):
+  return(x/y)
