977a5c8126e7af885204ecebfd057a8474441c32
kajal mishra <km3826219@gmail.com>
2026-04-08 11:04:25 +0530

    calculation1.py

--- data_visualization/classwork/module/calculation.py
+++ data_visualization/classwork/module/calculation.py
@@ -1,1 +1,6 @@
-
+#calculation1.py
+import numericCalculation
+m=10
+n=20
+print("sum of",m,"and",n,"is:",numericCalculation.calculateSum(m,n))
+print("product of",m,"and",n,"is:",numericCalculation.calculateMul(m,n))
