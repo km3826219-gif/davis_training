f77a923adb89534c68ec53c27d9a3e40153b3031
kajal mishra <km3826219@gmail.com>
2026-04-09 10:11:51 +0530

    Add files via upload

---
+++ data_visualization/lab_work/python_coding_challaenge-2/check vowel.py.py
@@ -0,0 +1,6 @@
+ch = input("Enter character: ")
+
+if ch in "aeiouAEIOU":
+    print("Vowel")
+else:
+    print("Not Vowel")
---
+++ data_visualization/lab_work/python_coding_challaenge-2/counts high value.py.py
@@ -0,0 +1,8 @@
+nums = [10,60,30,80]
+
+count = 0
+for i in nums:
+    if i > 50:
+        count += 1
+
+print("Count =", count)
